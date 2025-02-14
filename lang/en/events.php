06de6ac399ee417ef82c63b053ec9a68a69a6f4c
zaynom <ouaziz0626@gmail.com>
2025-02-14 16:04:13 +0100

    feat(i18n): add event crud messages translations for English and French

--- lang/en/events.php
+++ lang/en/events.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+    'not_found' => 'Event not found.',
+    'created' => 'Event created.',
+    'updated' => 'Event updated.',
+    'deleted' => 'Event deleted.',
     'joined' => 'You have successfully joined the event',
     'left' => 'You have successfully left the event',
     'full' => 'The event is full',
--- lang/fr/events.php
+++ lang/fr/events.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+    'not_found' => 'Événement non trouvé.',
+    'created' => 'Événement créé.',
+    'updated' => 'Événement mis à jour.',
+    'deleted' => 'Événement supprimé.',
     'joined' => 'Vous avez rejoint l\'événement avec succès',
     'left' => 'Vous avez quitté l\'événement avec succès',
     'full' => 'L\'événement est complet',
