5a61ee49b02a852ac8bc7ae2f7156939970ecee7
zaynom <ouaziz0626@gmail.com>
2025-02-14 19:13:10 +0100

    feat(i18n): add category crud messages translations for English and French

---
+++ lang/en/categories.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'not_found' => 'Category not found.',
+    'created' => 'Category created.',
+    'updated' => 'Category updated.',
+    'deleted' => 'Category deleted.',
+];
---
+++ lang/fr/categories.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'not_found' => 'Catégorie non trouvé.',
+    'created' => 'Catégorie créé.',
+    'updated' => 'Catégorie mis à jour.',
+    'deleted' => 'Catégorie supprimé.',
+];
